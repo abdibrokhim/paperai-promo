f8b50d152e236eaa875038cdbbf5430d2b183c11
abdibrokhim <abdibrokhim@gmail.com>
2024-09-21 19:05:20 +0500

    fix issue

--- src/app/products/app/page.tsx
+++ src/app/products/app/page.tsx
@@ -1,4 +1,4 @@
-import '../globals.css';
+import '../../globals.css';
 
 import Footer from '../../components/footer';
 import HeaderPrimary from '../../components/header.primary';
--- src/app/products/extension/page.tsx
+++ src/app/products/extension/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image"; 
-import '../globals.css';
+import '../../globals.css';
 
 import Footer from '../../components/footer';
 import HeaderPrimary from '../../components/header.primary';
